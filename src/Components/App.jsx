d7a2d3a5299c90018b17eb321e7776a1b382634c
Yanal-Sabbagh07 <Yanal-Sabbagh07@github.com>
2021-03-24 16:12:50 +0100

    styling and CreatArea component

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import CreateArea from "./CreateArea";
 function App(){
     return(
         <div>
             <Header />
+            <CreateArea />
             <Note />
             <Footer />
         </div>
--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -5,6 +5,7 @@ function Note(){
         <div className="note">
             <h1>this is the note title</h1>
             <p>this is the note content</p>
+            <button>Delete</button>
         </div>
     );
 }
